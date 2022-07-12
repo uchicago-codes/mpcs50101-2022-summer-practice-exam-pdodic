0599735ecd2c3d9d2a7f529617e7e57f9940c4aa
pdodic <107725401+pdodic@users.noreply.github.com>
2022-07-11 20:08:46 -0400

    Update problem2.py

--- problem2.py
+++ problem2.py
@@ -1,11 +1,12 @@
 # Write a PROGRAM to count the number of vowels in a word.
 
 def validate_input(word):
-    """Docstring here"""
-    #
-    # Your code here
-    #
-    return __a_bool__
+    """Validates Input"""
+    
+    if word islpha():
+        return True
+    else:
+        print("Input invalid.")
 
 def count_vowels(word):
     """Docstring here"""
@@ -15,9 +16,12 @@ def count_vowels(word):
     return __Something__Here__
 
 
+def main():
+    word = input("Enter a word:" )
+    
+    def validate_input(word)
+    def count_vowels(word)
 
-#
+
 if __name__ == "__main__":
-   #
-   #
-   #
+    def main()
